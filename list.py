d5c0f250aa234d51ba963433dfc31c6c61294a0d
Abdus Shohid Nasim <abdusshohidnasim@gmail.com>
2026-05-11 21:44:39 +0600

    vieo 26

---
+++ list.py
@@ -0,0 +1,30 @@
+list = ["Python", "C++", "C", "Dart", "Java"]
+print(list)
+print(list[0] ,"dfj")
+print(list[1] )
+print(list[-1] )
+print("C++" in list)
+print("C+" in list)
+print("C++"not  in list)
+print("C++"  in list)
+
+#list = list.append[2 , 3, "dfjk", 34, ]
+print(len(list), "lenth")
+list.append("Naiem hassan ") # last index added
+print(list)
+list.remove("C") # Remove list in C 
+list.sort() # list thaka shot korar neom 
+print(list) #
+list.reverse() # list revers kora 
+print(list)
+list.pop() # List index remove in list 
+print(list)
+list2 = list.copy()
+list.clear()
+print(list)
+print(list2.count("C++"))
+print("List Clear")
+print(list2)
+
+print(list2)
+
