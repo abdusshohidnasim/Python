5648b6715673e9a0ada583969f5ab97407fa5cd2
Abdus Shohid Nasim <abdusshohidnasim@gmail.com>
2026-06-27 12:27:37 +0600

    done
    done
    
    Co-Authored-By: ishtiaqalamnahid <286217504+ishtiaqalamnahid@users.noreply.github.com>

--- while_loop.py
+++ while_loop.py
@@ -9,17 +9,7 @@ if(i<0):
 if(i==100):
     print("The loop has ended.")
 print("The loop has ended.")
-print("The loop has ended.")
-print("Naiem Hassan Naiem")
-print("How are you?")
-print("I am fine.")
-print("what do you want to do?")
-print("I want to learn Python.")
-print("That's great! Python is a versatile programming language.")
-print("Yes, I have heard that Python is used in web development, data science, and artificial intelligence.")
-print("Absolutely! Python's simplicity and readability make it a popular choice for beginners and professionals alike.")
-print("I am excited to start learning Python and explore its various applications.")
-print("That's wonderful! There are many online resources and tutorials available to help you get started with Python.")
+
 
 
 
