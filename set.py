2aa2d34070705d77353055934ea0ca49ccff043a
Abdus Shohid Nasim <abdusshohidnasim@gmail.com>
2026-07-10 08:54:01 +0600

    comment out
    comment out
    
    Co-Authored-By: ishtiaqalamnahid <286217504+ishtiaqalamnahid@users.noreply.github.com>

--- set.py
+++ set.py
@@ -11,5 +11,5 @@ print(student1 ^ student2) # student1 and student2 er common
 print(student1.union(student2)) # student1 and student2 er value add korbe
 print(student1.intersection(student2)) # student1 and student2 er common value print korbe
 print(student1.difference(student2)) # student1 er value student2 er value theke remove kore print korbe
-print(student1.symmetric_difference(student2)) # student1 and student
-print(student1.isdisjoint(student2)) # student1 and student2 er common value ache kina check korbe
+print(student1.symmetric_difference(student2)) 
+print(student1.isdisjoint(student2))
